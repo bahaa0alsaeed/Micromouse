f8e68e99d98d065b09f4ba21ba9a0d0201dc9491
Bahaa Alsaeed <bahaa.xq00@gmail.com>
2026-08-27 19:36:23 +0300

    feat: add PID library and its test and add native env in .ini file

--- lib/PID/PID.cpp
+++ lib/PID/PID.cpp
@@ -2,13 +2,49 @@
 
 PID::PID(const PIDConfig& config)
 {
+    PID::config = config;
 }
 
-float PID::compute(float setpoint, float measurement, float deltaTime)
+float PID::compute(const float setpoint, const float measurement, float dt)
 {
-    return 0.0f;
+    const float error = setpoint - measurement;
+
+    // Accumulate and clamp integral to prevent windup
+    integral += error * dt;
+    if (integral > config.integralMax) integral = config.integralMax;
+    else if (integral < config.integralMin) integral = config.integralMin;
+
+    // Apply low-pass filter to derivative to reduce noise
+    if (dt == 0) dt = 0.001;
+    const float raw_derivative = (error - previousError) / dt;
+    const float derivative = config.alpha * previousDerivative +
+        (1 - config.alpha) * raw_derivative;
+
+    previousDerivative = derivative;
+    previousError = error;
+
+    // Combine P, I, and D components
+    const float raw_output = config.kp * error + config.ki * integral + config.kd * derivative;
+
+    if (raw_output > config.outputMax)
+        output = config.outputMax;
+    else if (raw_output < config.outputMin)
+        output = config.outputMin;
+    else
+        output = raw_output;
+
+    return output;
+}
+
+void PID::changeParameters(const PIDConfig& newConfig)
+{
+    config = newConfig;
 }
 
 void PID::reset()
 {
+    previousDerivative = 0.0;
+    previousError = 0.0;
+    output = 0.0;
+    integral = 0.0;
 }
--- lib/PID/PID.h
+++ lib/PID/PID.h
@@ -2,25 +2,29 @@
 
 struct PIDConfig
 {
-    float kp;
-    float ki;
-    float kd;
-    float outputMin;
-    float outputMax;
-    float integralMin;
-    float integralMax;
+    float kp = 0.0;
+    float ki = 0.0;
+    float kd = 0.0;
+    float alpha = 0.7;
+    float outputMin = 0.0;
+    float outputMax = 0.0;
+    float integralMin = 0.0;
+    float integralMax = 0.0;
 };
 
 class PID
 {
     PIDConfig config = {};
-    float previousError = 0;
-    float integral = 0;
-    float output = 0;
+
+    float previousError = 0.0;
+    float integral = 0.0;
+    float previousDerivative = 0.0;
+    float output = 0.0;
 
 public:
     explicit PID(const PIDConfig& config);
-    float compute(float setpoint, float measurement, float deltaTime);
+    float compute(float setpoint, float measurement, float dt);
+    void changeParameters(const PIDConfig& newConfig);
     void reset();
 };
 
